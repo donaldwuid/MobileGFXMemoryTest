f44481b82af7a00b271fadce5eb9e014e7c74af4
Donald Wu <wdo_o@hotmail.com>
2018-11-22 20:53:34 +0800

    commit test project and profile result

---
+++ Assets/LoadImage.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class LoadImage : MonoBehaviour {
+
+    public string path1;
+    private bool m_didLoad = false;
+    public void OnClick(BaseEventData e)
+    {
+        if(!m_didLoad)
+        {
+            m_didLoad = true;
+            GameObject prefab;
+            prefab = Resources.Load<GameObject>(path1);
+            Instantiate(prefab, transform);
+        }
+    }
+}
